90ffcb637f04537bd42f7b9bfb52718ea0a12ea9
shafrazi <shafrazi@live.com>
2020-09-26 17:31:09 +0530

    Updates

---
+++ precache-manifest.b11a97d4a6f25ec0b80dce0d7961efa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8d69bc4235705c905ba1ebe474dd40",
+    "url": "/odin-library-react/index.html"
+  },
+  {
+    "revision": "451a09b7ad8ac0392cd2",
+    "url": "/odin-library-react/static/css/main.c5998949.chunk.css"
+  },
+  {
+    "revision": "30b7393c5cf915ecfc7f",
+    "url": "/odin-library-react/static/js/2.6a80a6fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/odin-library-react/static/js/2.6a80a6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451a09b7ad8ac0392cd2",
+    "url": "/odin-library-react/static/js/main.8760cd86.chunk.js"
+  },
+  {
+    "revision": "7179a63f4289d3a369f8",
+    "url": "/odin-library-react/static/js/runtime-main.b0292c88.js"
+  }
+]);
